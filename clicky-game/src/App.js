e29ec78e9cde1f2716e37ca53c11c8769a5d0566
unknown <sherman.jonathan.r@gmail.com>
2019-06-27 22:29:46 -0700

    beginning files and some components added

---
+++ clicky-game/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CharacterCard from './components/CharacterCard';
+import Wrapper from './components/Wrapper';
+import characters from './character.json';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    character
+  }
+
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    )
+  }
+
+
+}
+
+export default App;
