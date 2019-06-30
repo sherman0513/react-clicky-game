c98e45015525dd851f2e3d06fc3c5dc459373bf7
unknown <sherman.jonathan.r@gmail.com>
2019-06-29 22:12:43 -0700

    changed col on wrapper

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,7 +4,7 @@ import "./wrapper.css";
 function Wrapper(props) {
     return (
         <div className="wrapper">
-            <div className="container col-md-3">
+            <div className="container col-md-6">
                 {props.children}
             </div>
         </div>
